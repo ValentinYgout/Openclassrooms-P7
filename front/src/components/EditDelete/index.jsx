3090545f495f97bfe9c73e8b5fdb31c247191a81
ValentinYgout <ValentinYgout@gmail.com>
2022-08-01 12:07:51 +0200

    slight improvements to EditDelete

--- front/src/components/EditDelete/index.jsx
+++ front/src/components/EditDelete/index.jsx
@@ -3,6 +3,7 @@ import { useState, } from 'react';
 import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
+import './style.css'
 
 
 
@@ -10,17 +11,19 @@ import { useNavigate } from 'react-router-dom';
 
 
 const EditDelete = (props) => {
-    const navigate =useNavigate()
-    const { user,getIdTokenClaims} = useAuth0();
-    const  isAuthor=  props.userId === user.sub?true:false
-    const isAdmin = user["Roles/roles"]?.find(x=>x === "Admin")?true:false
+    const navigate = useNavigate()
+    const { user, getIdTokenClaims } = useAuth0();
+    const isAuthor = props.userId === user.sub ? true : false
+    const isAdmin = user["userRoles"]?.find(x => x === "Admin") ? true : false
     console.log(getIdTokenClaims('roles'))
-    console.log(isAdmin,'is this account an admin??')
+    console.log(isAdmin, 'is this account an admin??')
     console.log(isAuthor, 'is author? can we render edit???????')
-    const isAllowedToEdit =isAdmin||isAuthor
-    console.log(isAllowedToEdit,"isallowedtoedittttt because", isAdmin, "isadmin",isAuthor,"isAuthor")
-    const {refreshPost,id}=props
-    const [isEditing,setIsEditing]= useState(false)
+    const isAllowedToEdit = isAdmin || isAuthor
+    console.log(isAllowedToEdit, "isallowedtoedittttt because", isAdmin, "isadmin", isAuthor, "isAuthor")
+    const { refreshPost, id } = props
+    const [isEditing, setIsEditing] = useState(false)
+    const [isDeleting, setIsDeleting] = useState(false)
+
     const [title, setTitle] = useState("");
     const [selectedFile, setSelectedFile] = useState("");
     const { getAccessTokenSilently } = useAuth0();
@@ -29,16 +32,16 @@ const EditDelete = (props) => {
     const handleDelete = async () => {
         const token = await getAccessTokenSilently();
         console.log(token);
-        
+
 
         axios.delete(`http://localhost:3500/api/post/${id}`, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
         })
-            .then(response =>(console.log('Delete successful'),
-            alert('The post was deleted, you will now be redirected to Home'),  navigate("/home"))
-            
+            .then(response => (console.log('Delete successful'),
+                alert('The post was deleted, you will now be redirected to Home'), navigate("/home"))
+
             )
             .catch(error => {
                 console.log(error.message);
@@ -46,29 +49,29 @@ const EditDelete = (props) => {
             });
     }
     const handleSendEdit = async () => {
-  
+
         const token = await getAccessTokenSilently();
-   
+
         let formData = new FormData();
-		formData.append("image", selectedFile);
-		formData.append("title", title);
+        formData.append("image", selectedFile);
+        formData.append("title", title);
         formData.append("selectedFile", selectedFile.name);
-   
-       
+
+
         axios.put('http://localhost:3500/api/post/' + props.id, formData, {
             headers: {
-				'Accept': 'application/json',
+                'Accept': 'application/json',
                 'Authorization': `Bearer ${token}`,
-				'Content-Type': 'multipart/form-data'
-        
-			}
+                'Content-Type': 'multipart/form-data'
+
+            }
         })
-            .then(response =>{
-                console.log(response,'Edit successful')
+            .then(response => {
+                console.log(response, 'Edit successful')
                 alert("post edited")
                 refreshPost()
-                
-            } )
+
+            })
             // window.location.reload()
 
             .catch(error => {
@@ -84,40 +87,43 @@ const EditDelete = (props) => {
         setIsEditing(false)
     }
 
-    
+
+    if (!isAllowedToEdit) {
+        return <></>
+    }
 
     return (
 
-        isAllowedToEdit?
-        isEditing?
-            <div className='EditDelete'>
-              <form>
-      <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+        <div>
+            {(!isEditing && !isDeleting) && <div className='EditDelete'>
+                <button className="" onClick={() => handleEdit()}>Edit</button>
+                <button className="" onClick={() => setIsDeleting(true)}>delete</button>
+            </div>}
 
-        <input
-          type="file"
-          onChange={(e) => setSelectedFile(e.target.files[0])}
-         
-        />
-      </form>
-                <button className="" onClick={() => handleCancelEdit()}>Cancel Edit</button>
-                <button className="" onClick={() => handleSendEdit()}>Confirm Edit</button>
-                <button disabled className="" onClick={() => handleDelete()}>delete</button>
-            </div>
-            :
-            <div className='EditDelete'>
-                <button className="" onClick={() => handleEdit()}>edit</button>
-                <button className="" onClick={() => handleDelete()}>delete</button>
-            </div>
-            :<></>
+            {isEditing && <div className='EditDelete'>
+                <form>
+                    <input
+                        type="text"
+                        placeholder='Enter new title here'
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
 
-       
+                    <input
+                        type="file"
+                        onChange={(e) => setSelectedFile(e.target.files[0])}
 
+                    />
+                </form>
+                <button className="editDeleteButton" onClick={() => handleCancelEdit()}>Cancel Edit</button>
+                <button className="editDeleteButton" onClick={() => handleSendEdit()}>Confirm Edit</button>
+            </div>}
 
+            {isDeleting && <div>
+                <button className="editDeleteButton" onClick={() => setIsDeleting(false)}>Cancel delete</button>
+                <button className="editDeleteButton" onClick={() => handleDelete()}>Confirm delete</button>
+            </div>}
+        </div>
     );
 };
 
